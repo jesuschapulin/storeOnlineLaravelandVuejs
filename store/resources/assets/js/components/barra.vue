5b5a74559505a40d96bf4d87a7d28136caca9c08
jesuschapulin <chaps.jesus.gz@gmail.com>
2018-09-11 16:30:42 -0500

    nuevo commit

--- store/resources/assets/js/components/barra.vue
+++ store/resources/assets/js/components/barra.vue
@@ -1,6 +1,6 @@
 <template>
         <nav class="shadow-lg navbar navbar-expand-lg navbar-light bg-light" id="barrita">
-		  <a class="navbar-brand" href="#">Store</a>
+		  <a class="navbar-brand" href="#">StoreOnline</a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
